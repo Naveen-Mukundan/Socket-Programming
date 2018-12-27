59a2bd6b3fb7b6b3099fffba924d76deacbcbb4a
Naveen Mukundan Ravindran <nxravindran@shockers.wichita.edu>
2018-12-26 21:18:21 -0600

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
+# Optional code to calculate the time taken for the entire socket programming in server side
 import time
 start=time.time()
+
 # Import socket module 
 import socket                
   
@@ -10,11 +12,15 @@ s = socket.socket()
 port = 8080    
   
 # connect to the server on local computer 
+# check using ifconfig or the alternative method according to your PC and then add the IP address here
 s.connect(('172.27.2.196', port)) 
   
 # receive data from the server 
 print s.recv(1024) 
+
 # close the connection 
-s.close()        
+s.close()       
+
+# Calculate total time taken and print it to the screen
 end=time.time()
 print(end-start)
